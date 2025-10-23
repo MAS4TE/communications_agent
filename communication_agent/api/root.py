02c3c6511952b9c70d0ed82efda09d360e88f3e3
ChriKo97 <c.komanns@proton.me>
2025-10-23 10:57:14 +0200

    case for no queue

--- communication_agent/api/root.py
+++ communication_agent/api/root.py
@@ -8,6 +8,7 @@ from fastapi import Request
 
 router = APIRouter()
 
+
 def create_bids(
     product_tuples,
 ) -> list[dict]:
@@ -15,13 +16,16 @@ def create_bids(
     # make this more than a placeholder function
     # use battery utility calculator and LLM
 
-    return [{
-        "start_time": product_tuples[0],
-        "end_time": product_tuples[1],
-        "volume": 0,
-        "price": 0,
-        "c_rate": 1,
-    }]
+    return [
+        {
+            "start_time": product_tuples[0],
+            "end_time": product_tuples[1],
+            "volume": 0,
+            "price": 0,
+            "c_rate": 1,
+        }
+    ]
+
 
 @router.get("/", response_class=FileResponse)
 def index():
@@ -29,17 +33,22 @@ def index():
     static_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), "static")
     return Path(static_dir) / "index.html"
 
-@router.get('/get_queue')
+
+@router.get("/get_queue")
 def some_router_function(request: Request):
+    if request.app.state.market_to_llm_queue is None:
+        return {200, "No queue"}
+
     market_message = request.app.state.market_to_llm_queue.get()
 
     if market_message["msg"] == "calculate bids":
         bids = create_bids(product_tuples=market_message["product_tuples"])
         print(f"LLM Bids: {bids}")
-        request.app.state.llm_to_market_queue.put({"msg": "calculated bids", "bids": bids})
+        request.app.state.llm_to_market_queue.put(
+            {"msg": "calculated bids", "bids": bids}
+        )
     elif market_message["msg"] == "market result":
         # feed_to_llm()
         print("Feeding to LLM")
 
     return 200
-
